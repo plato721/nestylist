89c6ebda02f339a5077de486639d775498d63a43
Matt Ewell <matt.ewell@gmail.com>
2020-03-08 14:10:55 -0600

    Add Item model

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe Item do
+  describe 'factory -' do
+    before :all do
+      @item = create :item
+    end
+
+    it 'is the thing' do
+      expect(@item.is_a? Item).to be_truthy
+    end
+
+    it 'has a name' do
+      expect(@item.name.is_a? String).to be_truthy
+      expect(@item.name).to_not be_blank
+    end
+  end
+end
