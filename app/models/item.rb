cc9cb22b2277842351aaa79f58e0b63ab7ca843f
Matt Ewell <matt.ewell@gmail.com>
2020-03-14 10:54:45 -0600

    Add table detail comment

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
+# create_table "items", force: :cascade do |t|
+#   t.string "name"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.bigint "user_id"
+#   t.index ["user_id"], name: "index_items_on_user_id"
+# end
 class Item < ApplicationRecord
   belongs_to :user, optional: true
 
